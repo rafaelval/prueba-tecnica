0028a52b3d3c7d6caff32680bb7a46e296f7cb16
rafaelval <artillero.satanas@gmail.com>
2025-04-28 15:15:20 -0500

    asignada estructura de carpetas y archivos inicial

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renderiza el texto principal', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/¡Hola, mundo!/i);
+  expect(headingElement).toBeInTheDocument();
 });
